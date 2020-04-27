b01594b32b9a3d7ec23968e2b6a85cc1bfa699ee
Devin_Moore <d.j.moore209@gmail.com>
2020-04-27 14:50:04 -0400

    registration is correctly making post request

--- anywhere-fitness/src/components/SignUpForm.js
+++ anywhere-fitness/src/components/SignUpForm.js
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
-import { axiosWithAuth } from '../utils/axiosWithAuth';
+import axios from 'axios'
 
 const SignUp = props => {
     const [ credentials, setCredentials ] = useState({
         username: '',
-        password: ''
+        email: '',
+        password: '',
+        roles: []
     });
 
     const handleChange = e => {
-        setCredentials({
+
+        if ( e.target.name === 'roles' ){
+          const rolesArray = ['init']
+          rolesArray.pop()
+          rolesArray.push(e.target.value)
+          setCredentials({
             ...credentials,
-            [e.target.name]: e.target.value
-        });
+            roles: rolesArray
+          })
+        } else {
+        
+          setCredentials({
+              ...credentials,
+              [e.target.name]: e.target.value
+          });
+        }
     };
 
-    const submit = e => {
+    const submit = async e => {
         e.preventDefault();
-        axiosWithAuth()
-            .post("/api/login", credentials)
+
+        axios
+            .post("https://anywherefitness-api.herokuapp.com/auth/register", credentials)
             .then(res => {
                 // res,data,payload
                 // redux - send the token to the redux store
@@ -26,27 +41,67 @@ const SignUp = props => {
                 // localStorage.setItem("token", JSON.stringify(res.data.payload));
                 // props.history.push("/friends-list");
                 console.log(res)
+                setCredentials({
+                  username: '',
+                  email: '',
+                  password: '',
+                  roles: []
+                })
             })
             .catch(err => console.log({ err }));
     };
 
     return (
       <div>
-        <h1>Sign-Up</h1>
+        <h1>Register</h1>
         <form onSubmit={submit}>
+
+          <label htmlFor='username'>Username</label>
           <input
+            id='username'
             type="text"
             name="username"
             value={credentials.username}
             onChange={handleChange}
           />
+
+          <label htmlFor='email'>Email</label>
           <input
+            id='email'
+            type="email"
+            name="email"
+            value={credentials.email}
+            onChange={handleChange}
+          />
+
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
             type="password"
             name="password"
             value={credentials.password}
             onChange={handleChange}
           />
-          <button>Register</button>
+
+          <label htmlFor='client'> Client </label>
+          <input 
+            type='radio'
+            id='client'
+            name='roles'
+            value='client'
+            onChange={handleChange}
+          />
+          
+          <label htmlFor='instructor'> Instructor </label>
+          <input 
+            type='radio'
+            id='instructor'
+            name='roles'
+            value='instructor'
+            onChange={handleChange}
+          />
+          
+          <button>Submit</button>
         </form>
       </div>
     )
