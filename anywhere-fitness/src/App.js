ef3409c9258e046db1cf04d67d2bc6ba4532795c
Gabriel Romero <gabester78@gmail.com>
2020-04-27 19:22:49 -0500

    Forms styled for desktop & mobible.

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import SignUpForm from "./components/SignUpForm";
