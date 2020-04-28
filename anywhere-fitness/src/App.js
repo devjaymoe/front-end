b2c12738fd8863c3511e07655838e58e2ddc8460
Gabriel Romero <gabester78@gmail.com>
2020-04-28 12:20:56 -0500

    Navbar set up for instructor and user accounts.

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -4,18 +4,18 @@ import { Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import SignUpForm from "./components/SignUpForm";
 import LoginForm from "./components/LoginForm";
+// import Classes from "./components/Classes";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-
       <Switch>
-        <Route path="/LoginForm" component={LoginForm} />
-        <Route exact path="/" component={SignUpForm} />
+        {/* <Route path="/classes" component={Classes} /> */}
+        <Route path="/register" component={SignUpForm} />
+        <Route exact path="/" component={LoginForm} />
       </Switch>
     </div>
   );
 }
-
 export default App;
