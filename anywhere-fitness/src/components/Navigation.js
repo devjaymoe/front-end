86e7e6da9b2cc5270f71aa797bc25baf1333792d
Gabriel Romero <gabester78@gmail.com>
2020-04-29 15:15:11 -0500

    Nav worked a bit.

--- anywhere-fitness/src/components/Navigation.js
+++ anywhere-fitness/src/components/Navigation.js
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navigation = (props) => {
-  return (
-    <nav>
-      <div className="navigation navSite">
-        <div>
-          <NavLink to="/">
-            <h1>Anywhere Fitness</h1>
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to="/About">About</NavLink>
-          <NavLink to="/register">Register</NavLink>
-        </div>
-      </div>
+  const [nav, setNav] = useState(true);
 
+  if (!nav) {
+    return (
       <div className="navLinksInstructor">
         <NavLink to="/Profile">Profile</NavLink>
         <NavLink to="/Dashboard">Dashboard</NavLink>
         <NavLink to="/CreateClass">Create a class</NavLink>
         <NavLink to="/ManageClass">Manage Classes</NavLink>
+      </div>
+    );
+  }
+
+  return (
+    <nav>
+      <div className="navigation navSite">
+        <div>
+          <NavLink to="/Classes">
+            <h1>Anywhere Fitness</h1>
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/Login">About</NavLink>
+          <NavLink to="/register">Register</NavLink>
+        </div>
       </div>
 
       <div className="navLinksUser">
