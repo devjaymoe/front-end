69b28b933cdd134b4e159b0497b1a81bcf9e5e74
Devin_Moore <d.j.moore209@gmail.com>
2020-04-30 17:31:52 -0400

    private route working as intended

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
-import PrivateRoute from './components/PrivateRoute';
 import Navigation from "./components/Navigation";
 import SignUpForm from "./components/SignUpForm";
 import LoginForm from "./components/LoginForm";
@@ -9,14 +8,15 @@ import Classes from "./components/Classes";
 import CreateClass from "./components/CreateClass";
 import DetailedClassCard from './components/DetailedClassCard';
 import EditClass from './components/EditClass'
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Switch>
-        <PrivateRoute exact path='/edit-class/:id' component={EditClass} />
-        <PrivateRoute exact path="/createclass" component={CreateClass} />
+        <PrivateRoute path='/edit-class/:id' component={EditClass} />
+        <PrivateRoute path="/createclass" component={CreateClass} />
         <Route path='/class-details/:id' component={DetailedClassCard} />
         <Route path='/classes' component={Classes} />
         <Route path="/register" component={SignUpForm} />
